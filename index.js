e9d1c854b0ada483e9130ed1d9950a0315a33273
Wela coder <ahmaddogar818@gmail.com>
2025-07-21 11:52:55 +0500

    index.js

--- index.js
+++ index.js
@@ -3,7 +3,19 @@ let buttons = document.querySelectorAll('button');
 
 let string = '';
 
-// Add event listener to each button
+//* Adjust the font size of the inputBox
+function adjustFontSize(value) {
+    const length = value.length;
+    if (length > 12) {
+        inputBox.style.fontSize = '20px';
+    } else if (length > 9) {
+        inputBox.style.fontSize = '28px';
+    } else {
+        inputBox.style.fontSize = '40px';
+    }
+};
+
+//* Add event listener to each button
 buttons.forEach(element => {
     element.addEventListener('click', (b) => {
 
@@ -11,30 +23,35 @@ buttons.forEach(element => {
         if (b.target.innerText === '=') {
             string = String(eval(string));
             inputBox.value = string;
+             adjustFontSize(string);
         }
 
         //* Add the text of the button to the input box
         else if (b.target.innerText == 'AC') {
             string = '';
             inputBox.value = string;
+             adjustFontSize(string);
         }
 
         //* Add the text of the button to the input box
         else if (b.target.innerText == 'DEL') {
             string = string.substring(0, string.length - 1);
             inputBox.value = string;
+             adjustFontSize(string);
         }
 
         //* Add the text of the button to the input box
         else if (b.target.id == 'plusMinus') {
-            string = String(-eqal(string));
+            string = String(-eval(string));
             inputBox.value = string;
+             adjustFontSize(string);
         }
 
         //* Add the text of the button to the input box
         else {
             string += b.target.innerText;
             inputBox.value = string;
+             adjustFontSize(string);
         }
 
         //* Update the input box with the new string
